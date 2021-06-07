8492df212f9dfd78c960f5007ccbf6afbd2bb015
libin9ijava <libin9ijava@163.com>
2021-06-07 13:10:02 +0800

    Initial commit

---
+++ testgit1/src/test/java/com/libin/testgit1/Testgit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libin.testgit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testgit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
